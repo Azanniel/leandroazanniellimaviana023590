29d8605030cf11c7ee7f5c0e7399dc17f62cb33e
azanniel <leo.azannielttt@gmail.com>
2026-01-27 23:14:20 -0400

    chore: eslint and Prettier configurations

---
+++ eslint.config.ts
@@ -0,0 +1,41 @@
+import js from '@eslint/js'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import pettierConfig from 'eslint-plugin-prettier/recommended'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+      pettierConfig,
+    ],
+    languageOptions: { ecmaVersion: 2020, globals: globals.browser },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'prettier/prettier': [
+        'error',
+        {
+          printWidth: 80,
+          tabWidth: 2,
+          singleQuote: true,
+          trailingComma: 'all',
+          arrowParens: 'always',
+          semi: false,
+          endOfLine: 'lf',
+        },
+      ],
+    },
+  },
+])
--- src/foo.tsx
+++ src/foo.tsx
@@ -1,3 +1,3 @@
 export function Foo() {
-  return <div>Foo Component</div>;
+  return <div>Foo Component</div>
 }
